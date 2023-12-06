99ac0e89c4505b580961f5971c8bce7208440fe0
frans <fransh@schoolmaster.nl>
2023-12-06 08:59:15 +0100

    little less brute force

--- python/day6.py
+++ python/day6.py
@@ -1,14 +1,18 @@
 with open("../input/day6.txt", "r", encoding="utf-8") as filehandle:
     aocinput = list(line.rstrip() for line in filehandle)
-times = [int(i) for i in aocinput[0].split(":")[1].split()] + [int("".join([i for i in aocinput[0].split(":")[1].split()]))]
-records = [int(i) for i in aocinput[1].split(":")[1].split()] + [int("".join([i for i in aocinput[1].split(":")[1].split()]))]
-a, b = 0, 0
+times = [int(i) for i in aocinput[0].split(":")[1].split()] + [
+    int("".join([i for i in aocinput[0].split(":")[1].split()]))
+]
+records = [int(i) for i in aocinput[1].split(":")[1].split()] + [
+    int("".join([i for i in aocinput[1].split(":")[1].split()]))
+]
+a, wins = 1, []
 for race in range(5):
-    t, r, wins = times[race], records[race], 0
+    t, r = times[race], records[race]
     for i in range(t + 1):
-        wins = wins + 1 if (t - i) * i > r else wins
-    if race == 4:
-        b = wins
-    else:
-        a = wins if a == 0 else a * wins
-print(a, b)
+        if (t - i) * i > r:
+            wins.append(t - (2 * i) + 1)
+            break
+for i in wins[:4]:
+    a *= i
+print(a, wins[-1])
